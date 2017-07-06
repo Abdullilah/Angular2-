841eee6163b685e5399ed70039eb0cfc9d6a8091
Abdullilah <abdullilaphd@hotmail.com>
2017-07-07 00:09:33 +0200

    ngIf, ngModle

--- dev/comp1.component.ts
+++ dev/comp1.component.ts
@@ -7,13 +7,25 @@ import {Component, Input} from 'angular2/core';
         <h3>Here are the data, called from the main cmoponent</h3>
         
         <ul>
-          <li *ngFor="#obj of info">
+          <li *ngFor="#obj of info" (click)="eleClicked(obj)">
             <div>Name: {{obj.name}}</div>
             <div>Age: {{obj.age}}</div>
+            <div>Email: {{obj.email}}</div>
+            <div>Phone: {{obj.phone}}</div>
           </li>
         </ul>
+        <div *ngIf="status">
+          <span>Email: </span><input type="text" [(ngModel)]="selectedItem.email">
+          <span>phone: </span><input type="text" [(ngModel)]="selectedItem.phone">
+        </div>
     `
 })
 export class Comp1Component {
   @Input() info : Array<{name: string, age: string, email: string, phone: string}> = [];
+  public status = false;
+  public selectedItem : {};
+  eleClicked(objectSel : {}){
+    this.selectedItem = objectSel;
+    this.status = true;
+  }
 }
