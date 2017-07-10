26e4224a6d3a8eaec17793294ce7778615e5e997
Abdullilah <abdullilaphd@hotmail.com>
2017-07-10 02:06:20 +0200

    Pipes

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -19,6 +19,24 @@ import {ContactSevice} from "./contact.service";
         <section>
           <router-outlet></router-outlet>
         </section>
+        <div>
+          <h1>Pipes:</h1>
+          <p>{{newName | uppercase}}</p>
+          <p>{{newName | lowercase}}</p>
+          <p>{{date | date}}</p>
+          <p>{{date | date: 'y-m-d'}}</p>
+          <p>{{date | date: 'short'}}</p>
+          <p>{{date | date: 'fullDate'}}</p>
+          <p>{{15.32 | number: '1.2-4'}}</p> <!-- 1.nim-max -->
+          <p>{{15.32 | number: '1.4-4'}}</p> <!-- 1.nim-max -->
+          <p>{{15.32 | currency}}</p>
+          <p>{{15.32 | currency: 'USD'}}</p>
+          <p>{{15.32 | currency: 'EUR'}}</p>
+          <p>{{15.32 | currency: 'USD': true}}</p>
+          <p>{{15.32 | currency: 'USD': true: '1.4-4'}}</p>
+          <!-- Stateful pipe: wait till something happend and then do it -->
+          <p>{{randomData | async}}</p>
+        </div>
     `,
   directives: [Comp1Component, Comp2Component, ROUTER_DIRECTIVES],
   styleUrls: ['../css/style.css']
@@ -29,6 +47,10 @@ import {ContactSevice} from "./contact.service";
   {path: '/page2', name: 'SecondPage', component: Comp2Component}
 ])
 export class AppComponent{
-
+  public newName = "Abdullilah";
+  date = new Date();
+  randomData = new Promise((resolve, reject) => {
+    setTimeout(()=>resolve("Random Data!"), 3000)
+  });
 }
 
